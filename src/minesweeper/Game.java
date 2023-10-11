3d87166c7253fdf9be4cce79db0594a65de6f4e7
Marcela Mejia <mejiao24@hotmail.com>
2023-10-11 19:16:23 +1100

    create initial structure

---
+++ src/minesweeper/Game.java
@@ -0,0 +1,37 @@
+package minesweeper;
+
+public class Game {
+	// methods I could need:
+	// startGame, selectBox, newGame, 
+	
+	private int[][] matrix = new int[10][10]; 
+	
+		
+	// initialise the game
+	public void startGame() {
+		// prepare the fields, matrix: 10 x 10 that contains 10 mines.
+		// ask the player to provide a coordinate to play.
+		// check if there is a mine at that coordinate.
+		// if the player is allowed to continue playing, show the matrix again,
+		// if the player loses show a message, if wins show another message.
+		// ask the player if wants to play again or exit the game.
+	}
+	
+	public void selectBox(){
+		
+	}
+	
+	public void newGame() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/minesweeper/Main.java
@@ -0,0 +1,22 @@
+package minesweeper;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// create a new game
+		Game game = new Game();
+		
+		// start playing
+		game.startGame();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
